91c12a9187b7027ac87ea03a21b22f3e7a656421
q-inho <f0402043@gmail.com>
2022-09-08 04:53:21 +0900

    skeleton code

--- main.py
+++ main.py
@@ -25,14 +25,12 @@ search = st.text_input('검색어를 입력하세요')
 
 # language option
 option = st.radio('언어', ('영어', '한글'), horizontal=True)
-style = df.style.hide_index() # hide index
-
 
 # output search result
 if df['영어'].str.contains(search, case=False).any() and search != '' and option == '영어':
-    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)].to_html(), unsafe_allow_html=True)
+    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)])
 elif df['번역문'].str.contains(search, case=False).any() and search != '' and option == '한글':
-    st._legacy_dataframe(df[df['번역문'].str.contains(search, case=False)].to_html(), unsafe_allow_html=True)
+    st._legacy_dataframe(df[df['번역문'].str.contains(search, case=False)])
 elif search == '':
     st.write('')
 else:
