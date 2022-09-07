19fa981e96774cb3607dba6e75b176ee4df7d147
q-inho <f0402043@gmail.com>
2022-09-08 02:47:06 +0900

    add search function

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import streamlit as st
-import csv
 
 # Title
 st.title('양자컴퓨터 문서번역 용어집')
 
 # Read data
 df = pd.read_csv('glossary.csv')
-sort_df=df.sort_values('영어') # 영어순으로 정렬
 
-#인덱스 숨기기
+# Search Function
+search = st.text_input('검색어를 입력하세요')
+if df['영어'].str.contains(search).any():
+    st.write(df[df['영어'].str.contains(search)])
+else:
+    st.write('검색어가 없습니다.')
+
+# Sort data
+sort_df=df.sort_values('영어') # Alphbetical order
+
+# Hide index
 # CSS to inject contained in a string
 hide_table_row_index = """
             <style>
