7a8f96a6d3915b60a8617003bb0623627b9b842b
q-inho <f0402043@gmail.com>
2022-09-08 02:32:29 +0900

    hide index

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ st.title('양자컴퓨터 문서번역 용어집')
 
 # Read data
 df = pd.read_csv('glossary.csv')
-sort_df=df.sort_values('영어')
+sort_df=df.sort_values('영어') # 영어순으로 정렬
+sort_df=sort_df.style.hide_index() # 인덱스 숨기기
 st.table(sort_df)
