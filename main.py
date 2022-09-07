1319017afdca64a7726e92f1654648cc9f7112fd
q-inho <f0402043@gmail.com>
2022-09-08 02:57:13 +0900

    test dataframe

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ search = st.text_input('검색어를 입력하세요')
 if df['영어'].str.contains(search).any() and search != '':
     st.dataframe(df[df['영어'].str.contains(search)])
 else:
-    st.dataframe('검색어가 없습니다.')
+    st.write('검색어가 없습니다.')
 
 st.header('전체 용어집')
 # Sort data
