c0549f2665a08b4b89927484e22a5da9dd10c3cd
q-inho <f0402043@gmail.com>
2022-09-08 04:04:49 +0900

    tset

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ else:
 #search input
 search = st.text_input('검색어를 입력하세요')
 if df['영어'].str.contains(search, case=False).any() and search != '' and option == '영어':
-    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)])
+    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)], width=1280)
 elif df['번역문'].str.contains(search, case=False).any() and search != '' and option == '한글':
     st._legacy_dataframe(df[df['번역문'].str.contains(search, case=False)])
 elif search == '':
