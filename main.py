dbf1ab238485b140e475304a18e6d90b7ff19bec
q-inho <f0402043@gmail.com>
2022-09-08 02:35:06 +0900

    test remove index

--- main.py
+++ main.py
@@ -8,5 +8,18 @@ st.title('양자컴퓨터 문서번역 용어집')
 # Read data
 df = pd.read_csv('glossary.csv')
 sort_df=df.sort_values('영어') # 영어순으로 정렬
-sort_df=sort_df.style.hide_index() # 인덱스 숨기기
-st.table(sort_df)
+
+#인덱스 숨기기
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
+st.table(df)
