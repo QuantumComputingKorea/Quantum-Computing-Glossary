53aad9e7ae9c0ccba8c8f7d6719ea76736438ce3
q-inho <f0402043@gmail.com>
2022-09-08 18:33:46 +0900

    test sorting

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ else:
 
 # Sort data
 sort_df=df.sort_values(by='영어', inplace=True, key=lambda col: col.str.lower()) # Alphbetical order
+st.table(sort_df)
 
 # Hide index
 # CSS to inject contained in a string
