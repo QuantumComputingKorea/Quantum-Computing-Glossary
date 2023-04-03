73a2da53d757c10aad8a923ff3bd2bca0ac43149
f0402043 <79438062+q-inho@users.noreply.github.com>
2023-04-04 03:10:10 +0900

    define Sessionstate variable

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ st.header('용어 검색')
 # Read data
 df = pd.read_csv('glossary.csv')
 
+
+# Define SessionState variable
+if "use_container_width" not in st.session_state:
+    st.session_state.use_container_width = True
+
 # Search Function
 #search input
 
@@ -25,9 +30,9 @@ option = st.radio('언어', ('영어', '한글'), horizontal=True)
 
 # output search result
 if df['영어'].str.contains(search, case=False).any() and search != '' and option == '영어':
-    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)],use_container_width=True)
+    st._legacy_dataframe(df[df['영어'].str.contains(search, case=False)],use_container_width=st.session_state.use_container_width)
 elif df['번역문'].str.contains(search, case=False).any() and search != '' and option == '한글':
-    st._legacy_dataframe(df[df['번역문'].str.contains(search, case=False)],use_container_width=True)
+    st._legacy_dataframe(df[df['번역문'].str.contains(search, case=False)],use_container_width=st.session_state.use_container_width)
 elif search == '':
     st.write('')
 else:
